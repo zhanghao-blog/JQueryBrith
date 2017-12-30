a97c9629a5666dd9d0dbbe0f89905bf1dc822531
zhanghao-blog <hazon311@gmail.com>
2017-12-30 21:22:38 +0800

    add diff-ask

--- JQuery.js
+++ JQuery.js
@@ -11,6 +11,13 @@
     $.fn = $.prototype;
 
     $.fn.init=function (selector,context) {
+        // 判断参入参数是否是dom对象
+        if(selector instanceof HTMLElement){
+            this.length=1;//构造对象的length必要
+            this[0]=selector;
+            return this;
+        }
+        console.log(3333)
         // querySelectorAll 兼容到ie8+
         var nodeList=(context||document).querySelectorAll(selector);
         this.length=nodeList.length;
@@ -20,7 +27,7 @@
         console.log(this)
         return this;
     }
-    //$.fn.init.prototype = $.fn;
+    $.fn.init.prototype = $.fn;
     $.fn.each=function (fn) {
         for(var i=0;i<this.length;i++){
             //fn为构造函数，call将this[i]指向fn并继承fn的属性，注意区别apply,apply以数组形式传递参数
@@ -30,6 +37,7 @@
     }
     $.fn.hide=function () {
         this.each(function () {
+            console.log(66666)
             this.style.display='none';
         })
     }
@@ -38,6 +46,5 @@
             this.style.display='block';
         })
     }
-
-     window.$=$;//打开接口方法
+     window.$=$;//仅将$暴露出去
 })()
